12281dec5a12caccf4394e4e6bccaa751984714d
mohitkotie <mohitkotie42@gmail.com>
2020-05-11 03:50:10 +0530

    initial commit

---
+++ src/main/java/com/atombot/app/ws/MobileAppWebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.atombot.app.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileAppWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileAppWebservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atombot/app/ws/MobileAppWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atombot.app.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileAppWebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
