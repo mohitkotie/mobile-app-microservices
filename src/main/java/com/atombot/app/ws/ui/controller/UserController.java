e48ab0588e5d8650c72abc3fa80beb2d7177e981
mohitkotie <mohitkotie42@gmail.com>
2020-05-11 21:43:08 +0530

    created package

--- src/main/java/com/atombot/app/ws/ui/controller/UserController.java
+++ src/main/java/com/atombot/app/ws/ui/controller/UserController.java
@@ -4,8 +4,11 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.atombot.app.ws.ui.model.request.UserDetailsRequestModel;
 
 @RestController
 @RequestMapping("users")  // http://localhost:8080/users
@@ -16,7 +19,7 @@ public class UserController {
 		return "get user was called";
 	}
 	@PostMapping
-	public String createUser() {
+	public String createUser(@RequestBody UserDetailsRequestModel userDetails) {
 		return "create user was called";
 	}
 	
